f87735be2fabf3f5f9788e9647b9c5210e299145
kosteckiann <kostecka@oregonstate.edu>
2018-08-12 13:39:26 -0700

    Add files via upload

---
+++ projects/kostecka/bridgetmcgDominion/unittest1.c
@@ -0,0 +1,58 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+int main() {
+
+	printf("Testing Compare function\n");
+	int larger = 1;
+	int smaller = -1;
+	int max = 2147483647;
+	int min = -2147483647;
+
+	//compare a > b
+	if ((compare(&larger, &smaller)) == 1)
+		printf("Compare passed when a > b\n");
+	else
+		printf("Compare failed when a > b\n");
+
+	//compare a < b
+	if ((compare(&smaller, &larger)) == -1)
+		printf("Compare passed when a < b\n");
+	else
+		printf("Compare failed when a < b\n");
+
+	//compare a = b
+	if ((compare(&larger, &larger)) == 0)
+		printf("Compare passed when a = b\n");
+	else
+		printf("Compare failed when a = b\n");
+
+	//a = min boundary
+	if ((compare(&min, &smaller)) == -1)
+		printf("Compare passed when a is minimum boundary\n");
+	else
+		printf("Compare failed when a is minimum boundary\n");
+
+	//a = max boundary
+	if ((compare(&max, &smaller)) == 1)
+		printf("Compare passed when a is maximum boundary\n");
+	else
+		printf("Compare failed when a is maximum boundary\n");
+
+	//b = min boundary
+	if ((compare(&larger, &min)) == 1)
+		printf("Compare passed when b is minimum boundary\n");
+	else
+		printf("Compare failed when b is minimum boundary\n");
+
+	//b = max boundary
+	if ((compare(&larger, &max)) == -1)
+		printf("Compare passed when b is maximum boundary\n");
+	else
+		printf("Compare failed when b is maximum boundary\n");
+}
---
+++ projects/kostecka/bridgetmcgDominion/unittest2.c
@@ -0,0 +1,71 @@
+#include "dominion.h"
+#include "dominion_helpers.h"
+#include <string.h>
+#include <stdio.h>
+#include <assert.h>
+#include "rngs.h"
+#include <stdlib.h>
+
+int main() {
+
+	printf("Testing updateCoins()\n");
+
+	int thisPlayer = 0;
+	struct gameState G, testG;
+	int seed = 1000;
+	int bonus = 0;
+	int copper = 0;
+	int silver = 0;
+	int gold = 0;
+	int k[10] = { adventurer, gardens, embargo, village, minion, mine, cutpurse, sea_hag, tribute, smithy };
+
+	initializeGame(2, k, seed, &G);
+
+	memcpy(&testG, &G, sizeof(struct gameState));
+
+	
+
+//negative bonus
+	G.coins = 0;
+	testG.coins = 0;
+	bonus = -5;
+	G.hand[thisPlayer][0] = copper;
+	G.hand[thisPlayer][1] = silver;
+	G.hand[thisPlayer][2] = gold;
+	updateCoins(thisPlayer, &G, -5);
+
+	printf("number of coins = %d, expected = %d\n", G.coins, testG.coins + 1 + 2 + 3 + bonus);
+
+
+//1 of each card
+	G.coins = 0;
+	testG.coins = 0;
+	bonus = 0;
+	G.hand[thisPlayer][0] = copper;
+	G.hand[thisPlayer][1] = silver;
+	G.hand[thisPlayer][2] = gold;
+	updateCoins(thisPlayer, &G, 0);
+
+	printf("number of coins = %d, expected = %d\n", G.coins, testG.coins + 1 + 2 + 3 + bonus);
+
+
+//0, 0, 1
+	G.coins = 0;
+	testG.coins = 0;
+	bonus = 0;
+	G.hand[thisPlayer][0] = gold;
+	updateCoins(thisPlayer, &G, 0);
+
+	printf("number of coins = %d, expected = %d\n", G.coins, testG.coins + 3 + bonus);
+
+
+//0, 0 , 0
+	G.coins = 0;
+	testG.coins = 0;
+	bonus = 0;
+	updateCoins(thisPlayer, &G, 0);
+
+	printf("number of coins = %d, expected = %d\n", G.coins, testG.coins + bonus);
+
+
+}
